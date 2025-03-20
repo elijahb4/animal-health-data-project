521693f098642b06c99775a5181d747edad2ca2a
James <jamesjustincase@gmail.com>
2025-03-20 16:38:18 +0000

    dashboard linked to other pages

--- components/navbar_temp.php
+++ components/navbar_temp.php
@@ -5,7 +5,7 @@
     <nav class="sidebar bg-light">
                     <div class="nav-header d-flex align-items-center justify-content-right">
                         <img
-                            src="assets/Elanco.svg"
+                            src="../assets/Elanco.svg"
                             class="img-fluid"
                             alt=""
                         /> 
@@ -13,27 +13,27 @@
                     <div class="position-sticky pt-3">
                         <ul class="nav flex-column">
                             <li class="nav-item">
-                                <a class="nav-link" href="#">
+                                <a class="nav-link" href="dashboard.php">
                                     Dashboard
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link active" href="#">
+                                <a class="nav-link" href="analytics.php">
                                     Analytics
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">
+                                <a class="nav-link" href="our_dogs.php">
                                     Our Dogs
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">
+                                <a class="nav-link" href="records.php">
                                     Records
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">
+                                <a class="nav-link" href="health_data.php">
                                     Health Data
                                 </a>
                             </li>
