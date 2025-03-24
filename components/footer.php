bf3de2defaeec6ff94686cbb4aadc819b7d19787
Elijah <elijah.j.bennett@gmail.com>
2025-03-24 18:48:37 +0000

    External Link Added

--- components/footer.php
+++ components/footer.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="css/global.css">
 <link rel="stylesheet" href="css/footer.css">
 <div class="footer">
-  <img src="assets/elanco-blue.svg" alt="Elanco" class="footer-logo">
+  <a href="https://elanco.com/" target="_blank"><img src="assets/elanco-blue.svg" alt="Elanco" class="footer-logo"></a>
 </div>
