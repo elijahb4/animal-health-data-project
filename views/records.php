5c8e85ed8cbfa7d1714d76c8adc1326cfcc27607
Elijah <elijah.j.bennett@gmail.com>
2025-04-09 17:22:32 +0100

    began work on raw csv viewing table

---
+++ views/records.php
@@ -0,0 +1,15 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>CSV Table Viewer</title>
+    <link rel="stylesheet" href="/../css/records.css">
+</head>
+<body>
+    <input type="file" id="csvFile" accept=".csv" />
+    <table id="csvTable"></table>
+<script src="/../js/records.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/papaparse@5.4.1/papaparse.min.js"></script>
+</body>
+</html>
