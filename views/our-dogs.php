5e40d412e5a539a75aa190bed103ff263fa9dc42
Kevin <kevsaxton7@gmail.com>
2025-04-19 17:25:53 +0100

    smol changes

--- views/our-dogs.php
+++ views/our-dogs.php
@@ -1,4 +1,4 @@
 
 <div class="view-container">
-    <img src="/../assets/content/45ab95c89eed0a9a78896f6460dd1cc1.jpg"/>
+    <img src="assets/content/45ab95c89eed0a9a78896f6460dd1cc1.jpg"/>
 </div>
