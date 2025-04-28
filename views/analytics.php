33bcc0d08d8ef7bd2af3f7e423a75cfbc7879468
Kevin <kevsaxton7@gmail.com>
2025-04-28 17:51:17 +0100

    card styles in elijahs epic work

--- views/analytics.php
+++ views/analytics.php
@@ -7,45 +7,47 @@
 </head>
 <body>
     <div class="view-container">
-        <div id=form-container>
-        <form id="dataQuery">
-            <div class="form-group">
-                <label for="selectDog">Select Dog:</label>
-                <select id="selectDog" required="yes"></select>
+        <div class="card">
+            <div id=form-container>
+            <form id="dataQuery">
+                <div class="form-group">
+                    <label for="selectDog">Select Dog:</label>
+                    <select id="selectDog" required="yes"></select>
+                </div>
+                <div class="form-group">
+                    <label for="datePicker">Select Date:</label>
+                    <input type="date" id="datePicker" min="2021-01-01" max="2023-12-31" />
+                </div>
+                <div class="form-group">
+                    <label>Data Points:</label>
+                    <button type="button" id="expandCheckboxes">Show Options ▼</button>
+                    <div id="checkboxContainer"></div>
+                </div>
+                <div class="form-group">
+                    <label for="chartTypes">Chart Type:</label>
+                    <select id="chartTypes" required></select>
+                </div>
+                <div class="form-group">
+                    <label for="rangeDays">Number of Days:</label>
+                    <input type="number" id="rangeDays" name="rangeDays" min="1" max="30" value="1" />
+                </div>
+                <div class="form-group">
+                    <label for="submit"><br /></label>
+                    <button id="submit" class="btn" type="submit">Load Chart</button>
+                </div>
+            </form>
             </div>
-            <div class="form-group">
-                <label for="datePicker">Select Date:</label>
-                <input type="date" id="datePicker" min="2021-01-01" max="2023-12-31" />
+            <br />
+            <div id="chart-container">
+                <div id="download-button-container">
             </div>
-            <div class="form-group">
-                <label>Data Points:</label>
-                <button type="button" id="expandCheckboxes">Show Options ▼</button>
-                <div id="checkboxContainer"></div>
+            <canvas id="myChart" width="400" height="400"></canvas>
             </div>
-            <div class="form-group">
-                <label for="chartTypes">Chart Type:</label>
-                <select id="chartTypes" required></select>
-            </div>
-            <div class="form-group">
-                <label for="rangeDays">Number of Days:</label>
-                <input type="number" id="rangeDays" name="rangeDays" min="1" max="30" value="1" />
-            </div>
-            <div class="form-group">
-                <label for="submit"><br /></label>
-                <button id="submit" class="btn" type="submit">Load Chart</button>
-            </div>
-        </form>
+            <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.8/dist/chart.umd.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
+            <script src="../js/getDogs.js"></script>
+            <script src="../js/charts.js"></script>
         </div>
-        <br />
-        <div id="chart-container">
-            <div id="download-button-container">
-        </div>
-        <canvas id="myChart" width="400" height="400"></canvas>
-        </div>
-        <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.8/dist/chart.umd.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
-        <script src="../js/getDogs.js"></script>
-        <script src="../js/charts.js"></script>
     </div>
 </body>
 </html>
--- views/records.php
+++ views/records.php
@@ -5,32 +5,34 @@
 </head>
 <body>
 <div class="view-container">
-    <div id="form-container">
-        <form id="dataQuery">
-        <div class="form-group">
-            <label for="selectDog">Select Dog:</label>
-            <select id="selectDog" required></select>
+    <div class="card">
+        <div id="form-container">
+            <form id="dataQuery">
+            <div class="form-group">
+                <label for="selectDog">Select Dog:</label>
+                <select id="selectDog" required></select>
+            </div>
+            <div class="form-group">
+                <label for="datePicker">Select Date:</label>
+                <input type="date" id="datePicker" min="2021-01-01" max="2023-12-31" />
+            </div>
+            <div class="form-group">
+                <label for="rangeDays">Number of Days:</label>
+                <input type="number" id="rangeDays" name="rangeDays" min="1" max="30" value="1" />
+            </div>
+            <div class="form-group">
+            <label for="submit"><br /></label>
+                <button id="submit" class="btn" type="submit">View Records</button>
+            </div>
+            </form>
+        <br />
+        <div id="chart-container">
+            <div id="download-button-container">
         </div>
-        <div class="form-group">
-            <label for="datePicker">Select Date:</label>
-            <input type="date" id="datePicker" min="2021-01-01" max="2023-12-31" />
+        <br />
+        <div id="data-container">
+            <table id="csvTable"></table>
         </div>
-        <div class="form-group">
-            <label for="rangeDays">Number of Days:</label>
-            <input type="number" id="rangeDays" name="rangeDays" min="1" max="30" value="1" />
-        </div>
-        <div class="form-group">
-        <label for="submit"><br /></label>
-            <button id="submit" class="btn" type="submit">View Records</button>
-        </div>
-        </form>
-    <br />
-    <div id="chart-container">
-        <div id="download-button-container">
-    </div>
-    <br />
-    <div id="data-container">
-        <table id="csvTable"></table>
     </div>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/papaparse@5.4.1/papaparse.min.js"></script>
