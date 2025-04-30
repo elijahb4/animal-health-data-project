e2c9a60c3460e1c9021c1282952cc3e8b4f74e60
Elijah <elijah.j.bennett@gmail.com>
2025-04-30 18:01:23 +0100

    404 revision

--- views/404.php
+++ views/404.php
@@ -7,14 +7,14 @@ http_response_code(404);
     <title>Page not found</title>
 </head>
 <body>
-    <div class="dashboard-container">
+    <div class="view-container">
     <h1>404: Page not found</h1>
     <p>The page or asset you're looking for does not exist in this location.</p>
     <button id="home_link" class="<?= ($_GET['page'] ?? 'dashboard') == 'dashboard' ? 'active' : '' ?>">
         <a href="index.php?page=dashboard">Click here to return to the dashboard</a>
     </button>
     <br />
-    <img width="600" height="auto" src="/../assets/content/GWFbN43WsAAAmLv.jpeg"/>
+    <img width="600" height="auto" src="../assets/content/45ab95c89eed0a9a78896f6460dd1cc1.jpg"/>
     </div>
 </body>
 </html>
