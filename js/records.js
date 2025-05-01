d30cef1aa0135fde5ed76bfeb5fb1dd9711f1008
Sanakan <99808919+Alchemane@users.noreply.github.com>
2025-05-01 14:12:15 +0100

    pathing updated to work with xampp

--- js/records.js
+++ js/records.js
@@ -1,5 +1,5 @@
 let currentData = [];
-const CSV_PATH = '/../database/activityData.csv';
+const CSV_PATH = '../database/activityData.csv';
 let minDate = null;
 let maxDate = null;
 
@@ -34,7 +34,7 @@ async function getDogs () {
 
 async function fetchDates() {
   try {
-      const response = await fetch('/../php_scripts/getDates.php');
+      const response = await fetch('../php_scripts/getDates.php');
       const data = await response.json();
       console.log("Min Date:", data.minDate, "Max Date:", data.maxDate);
       if (data.error) {
